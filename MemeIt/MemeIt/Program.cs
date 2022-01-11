f6d84d4189f65a5ef32fe4fb61a7ad77057f2dac
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-11 16:16:03 +0100

    feat: basic setup implemented

---
+++ MemeIt/MemeIt/Program.cs
@@ -0,0 +1,95 @@
+using System.Text;
+using MemeIt.Data;
+using MemeIt.Data.Serices;
+using MemeIt.Data.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("JWTSecretKey"))
+            )
+        };
+    });
+
+
+// todo set CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        x =>
+        {
+            x.AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithOrigins("http://localhost:3000");
+        });
+});
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddDbContext<AppDbContext>();
+
+// todo Repositorys/Services
+
+builder.Services.AddSingleton<IAuthService>(
+    new AuthService(
+        builder.Configuration.GetValue<string>("JWTSecretKey"),
+        builder.Configuration.GetValue<int>("JWTLifespan")
+    )
+);
+
+
+builder.Services
+    .AddControllers()
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        options.SerializerSettings.Converters.Add(new StringEnumConverter());
+    });
+
+var app = builder.Build();
+
+DbInitializer.CreateDbIfNotExists(app);
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+app.Run();
