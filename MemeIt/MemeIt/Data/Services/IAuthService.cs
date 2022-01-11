5285dd0538950b05488c4fc36a048a04ec5403de
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-11 16:18:05 +0100

    feat: Authentication infrastructure partly implemented

---
+++ MemeIt/MemeIt/Data/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using MemeIt.Models.DTOs;
+
+namespace MemeIt.Data.Serices;
+
+public interface IAuthService
+{
+    AuthData GetAuthData(int id, string role);
+    string HashPassword(string password);
+    bool VerifyPassword(string? actualPassword, string hashedPassword);
+}
---
+++ MemeIt/MemeIt/Models/DTOs/AuthData.cs
@@ -0,0 +1,9 @@
+﻿namespace MemeIt.Models.DTOs;
+
+public class AuthData
+{
+    public string Token { get; set; } = null!;
+    public long TokenExpirationTime { get; set; }
+    public int Id { get; set; }
+    public string UserRole { get; set; } = null!;
+}
