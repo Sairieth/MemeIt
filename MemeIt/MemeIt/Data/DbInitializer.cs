b1ad55e72fd22795fd7bd7e75322583d1525b63b
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-11 16:16:42 +0100

    feat: db initializer partly implemented

---
+++ MemeIt/MemeIt/Data/DbInitializer.cs
@@ -0,0 +1,36 @@
+﻿using MemeIt.Data.Serices;
+
+namespace MemeIt.Data;
+
+public static class DbInitializer
+{
+    private static void Initialize(AppDbContext context, IAuthService service)
+    {
+        context.Database.EnsureCreated();
+
+        // Look for any Entity.
+        //    if (context.entity.Any())
+        //    {
+        //        return; // DB has been seeded
+        //    }
+    }
+
+    public static void CreateDbIfNotExists(IHost host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<AppDbContext>();
+                var authService = services.GetRequiredService<IAuthService>();
+                DbInitializer.Initialize(context, authService);
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(ex, "An error occurred creating the DB.");
+            }
+        }
+    }
+}
