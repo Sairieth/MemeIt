db7d3a4995c09c1893f6ea367164b2d4357a0612
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-13 13:20:45 +0100

    mod: modifie two properties

--- MemeIt/MemeIt/Models/Entities/Comment.cs
+++ MemeIt/MemeIt/Models/Entities/Comment.cs
@@ -5,7 +5,7 @@ namespace MemeIt.Models.Entities;
 public class Comment : EntityWithModificationDate
 {
     public string Note { get; set; } = null!;
-    public string Tag { get; set; } = null!;
+    public string? Flag { get; set; } = default;
     public Meme Meme { get; set; } = null!;
-    public User User { get; set; } = null!;
+    public long UserId { get; set; }
 }
