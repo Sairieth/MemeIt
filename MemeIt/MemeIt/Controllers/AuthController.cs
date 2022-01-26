a8e905125ebc879934f7db1ddb0947c978da5d03
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-26 17:05:46 +0100

    feat: impl login/register

--- MemeIt/MemeIt/Controllers/AuthController.cs
+++ MemeIt/MemeIt/Controllers/AuthController.cs
@@ -1,13 +1,16 @@
-﻿using MemeIt.Core;
+﻿using System.Net;
+using MemeIt.Core;
 using MemeIt.Models.DTOs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using MemeIt.Infrastructure;
+using Newtonsoft.Json;
 
 namespace MemeIt.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
@@ -20,7 +23,7 @@ namespace MemeIt.Controllers
         }
 
         [HttpPost]
-        [Route("/login")]
+        [Route("login")]
         public async Task<ActionResult<AuthData>> Login(LoginData credentials)
         {
             if (!ModelState.IsValid) return BadRequest("missing passowrd/username");
@@ -29,17 +32,37 @@ namespace MemeIt.Controllers
 
             if (user == null)
             {
-                return BadRequest(new { error = "invalid username/password" });
+                return BadRequest(new {error = "invalid username/password"});
             }
 
             var passwordValid = _authService.VerifyPassword(credentials.Password, user.HashedPassword);
+
             if (!passwordValid)
             {
-                return BadRequest(new { error = "invalid username/password" });
+                return BadRequest(new {error = "invalid username/password"});
             }
 
-            return _authService.GetAuthData(user.Id, user.Role.Title!);
+            var authData = _authService.GetAuthData(user.Id, user.Role);
 
+            return Accepted(authData);
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<ActionResult<AuthData>> Register(RegisterUserDto registerData)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            var emailUniq = await _userRepository.IsEmailUniqueAsync(registerData.Email);
+            if (!emailUniq) return BadRequest(new { error = "user with this email already exists" });
+
+            var usernameUniq = await _userRepository.IsUsernameUniqueAsync(registerData.UserName);
+            if (!usernameUniq) return BadRequest(new { error = "user with this email already exists" });
+
+            var user = registerData.ToUser();
+            await _userRepository.AddUserAsync(user);
+            var authData = _authService.GetAuthData(user.Id, user.Role);
+            return Created("/api/auth/register",authData);
         }
     }
 }
