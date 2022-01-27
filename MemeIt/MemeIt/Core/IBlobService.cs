1462a934f43d29e26bc3881c15b845b8c6202b33
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-27 16:41:31 +0100

    feat: imp CRUD metods for AzureBlobStorage

---
+++ MemeIt/MemeIt/Core/IBlobService.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Blobs.Models;
+
+namespace MemeIt.Core;
+
+public interface IBlobService
+{
+    public Task<string> GetBlobUriAsync(string fileName, string containerName);
+    public Task<bool> UploadBlobAsync(string fileName,IFormFile file, string containerName);
+    public Task<bool> DeleteBlobAsync(string fileName, string containerName);
+
+}
---
+++ MemeIt/MemeIt/Data/Services/BlobService.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using MemeIt.Core;
+using MemeIt.Models.DTOs;
+
+namespace MemeIt.Data.Services;
+
+public class BlobService : IBlobService
+{
+    private readonly BlobServiceClient _blobServiceClient;
+
+    public BlobService(BlobServiceClient blobServiceClient)
+    {
+        _blobServiceClient = blobServiceClient;
+    }
+
+    public async Task<string> GetBlobUriAsync(string fileName, string containerName)
+    {
+        var containerClient = await Task.Run(() => _blobServiceClient.GetBlobContainerClient(containerName));
+        var blobClient = await Task.Run(() => containerClient.GetBlobClient(fileName));
+
+        return blobClient.Uri.AbsoluteUri;
+    }
+
+    public async Task<bool> UploadBlobAsync(string fileName, IFormFile file, string containerName)
+    {
+        var containerClient = await Task.Run(() => _blobServiceClient.GetBlobContainerClient(containerName));
+        var blobClient = await Task.Run(() => containerClient.GetBlobClient(fileName));
+        var httpHeaders = new BlobHttpHeaders()
+        {
+            ContentType = file.ContentType
+        };
+
+        var res = await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+
+        if (res != null) return true;
+
+        return false;
+    }
+
+    public async Task<bool> DeleteBlobAsync(string fileName, string containerName)
+    {
+        var containerClient = await Task.Run(() => _blobServiceClient.GetBlobContainerClient(containerName));
+        var blobClient = await Task.Run(() => containerClient.GetBlobClient(fileName));
+        return await blobClient.DeleteIfExistsAsync();
+    }
+}
