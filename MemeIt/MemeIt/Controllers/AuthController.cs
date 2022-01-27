477b117bdfefd2af8a0943bc39acc4e89761beff
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-27 09:33:22 +0100

    refactor: format file

--- MemeIt/MemeIt/Controllers/AuthController.cs
+++ MemeIt/MemeIt/Controllers/AuthController.cs
@@ -29,18 +29,10 @@ namespace MemeIt.Controllers
             if (!ModelState.IsValid) return BadRequest("missing passowrd/username");
 
             var user = await _userRepository.GetUserByUsernameAsync(credentials.Username);
-
-            if (user == null)
-            {
-                return BadRequest(new {error = "invalid username/password"});
-            }
+            if (user == null) return BadRequest(new {error = "invalid username/password"});
 
             var passwordValid = _authService.VerifyPassword(credentials.Password, user.HashedPassword);
-
-            if (!passwordValid)
-            {
-                return BadRequest(new {error = "invalid username/password"});
-            }
+            if (!passwordValid) return BadRequest(new {error = "invalid username/password"});
 
             var authData = _authService.GetAuthData(user.Id, user.Role);
 
@@ -60,8 +52,11 @@ namespace MemeIt.Controllers
             if (!usernameUniq) return BadRequest(new {error = "user with this email already exists"});
 
             var user = registerData.ToUser();
+
             await _userRepository.AddUserAsync(user);
+
             var authData = _authService.GetAuthData(user.Id, user.Role);
+
             return Created("/api/auth/register", authData);
         }
     }
