5d3c333dcc91838a93680bafe1e0609ec113ff1e
Henrik Märcz <henrik.riki.marcz@gmail.com>
2022-01-28 11:33:37 +0100

    feat: imp endpoints

--- MemeIt/MemeIt/Controllers/MemeController.cs
+++ MemeIt/MemeIt/Controllers/MemeController.cs
@@ -2,28 +2,31 @@
 using MemeIt.Core;
 using MemeIt.Infrastructure;
 using MemeIt.Models.DTOs;
+using MemeIt.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MemeIt.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/memes")]
     [ApiController]
     public class MemeController : ControllerBase
     {
         private readonly IBlobService _blobService;
         private readonly IMemeRepository _memeRepository;
+        private readonly IUserRepository _userRepository;
         private const string ImgContainerName = "images";
 
-        public MemeController(IBlobService blobService, IMemeRepository memeRepository)
+        public MemeController(IBlobService blobService, IMemeRepository memeRepository, IUserRepository userRepository)
         {
             _blobService = blobService;
             _memeRepository = memeRepository;
+            _userRepository = userRepository;
         }
 
         [HttpPost]
         [Route("post")]
-        public async Task<ActionResult> PostMeme(PostDataDto post)
+        public async Task<ActionResult> PostMeme([FromBody] PostDataDto post)
         {
             if (!ModelState.IsValid) return BadRequest();
             if (post.File == null || post.File.Length > 1) return BadRequest();
@@ -31,16 +34,81 @@ namespace MemeIt.Controllers
             var fileName = Guid.NewGuid().ToString() + Path.GetExtension(post.File.FileName);
 
             var result = await _blobService.UploadBlobAsync(fileName, post.File, ImgContainerName);
+            var user = await _userRepository.GetUserAsync(post.UserId);
 
-            var imgUri = await _blobService.GetBlobUriAsync(fileName, ImgContainerName);
-
-            if (result)
+            if (result && user != null)
             {
-                await _memeRepository.AddMemeAsync(post.ToMeme(imgUri));
+                var imgUri = await _blobService.GetBlobUriAsync(fileName, ImgContainerName);
+                await _memeRepository.AddMemeAsync(post.PostDataDtoToMeme(imgUri, user));
                 return Ok();
             }
 
             return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("{tag}")]
+        public async Task<ActionResult<List<MemeDto>>> GetMemesByTag([FromRoute] string tag)
+        {
+            var memes = await _memeRepository.GetMemesByTagAsync(tag);
+
+            var memeDtosByTag = memes?.Select(x => x.MemeToMemeDto()).ToList();
+
+            if (memeDtosByTag == null) return BadRequest("No memes by tag");
+
+            return Ok(memeDtosByTag);
+        }
+
+        [HttpGet]
+        [Route("user/{userId:long}")]
+        public async Task<ActionResult<List<MemeDto>>> GetMemesByUserId([FromRoute] long userId)
+        {
+            var memes = await _memeRepository.GetUsersMemesAsync(userId);
+
+            var memeDtosByTag = memes?.Select(x => x.MemeToMemeDto()).ToList();
+
+            if (memeDtosByTag == null) return BadRequest("No memes by user");
+
+            return Ok(memeDtosByTag);
+        }
+
+        [HttpPut]
+        [Route("update/{memeId:long}")]
+        public async Task<ActionResult> EditMemeTitle([FromRoute] long memeId, [FromBody] string newTitle)
+        {
+            var meme = await _memeRepository.GetMemeByIdAsync(memeId);
+
+            if (meme == null) return BadRequest("No meme was found");
+
+            await _memeRepository.EditMemeTitleAsync(newTitle, memeId);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        [Route("update/{memeId:long}")]
+        public async Task<ActionResult> EditMemeTag([FromRoute] long memeId, [FromBody] string newTag)
+        {
+            var meme = await _memeRepository.GetMemeByIdAsync(memeId);
+
+            if (meme == null) return BadRequest("No meme was found");
+
+            await _memeRepository.EditMemeTagAsync(newTag, memeId);
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        [Route("remove/{memeId:long}")]
+        public async Task<ActionResult> DeleteMeme([FromRoute] long memeId)
+        {
+            var meme = await _memeRepository.GetMemeByIdAsync(memeId);
+
+            if (meme == null) return BadRequest("No meme was found");
+
+            await _memeRepository.RemoveMemeAsync(memeId);
+
+            return Ok("OK");
         }
     }
 }
